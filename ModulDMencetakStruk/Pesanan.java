6a813da5976a2e436406b64a8021e55f1f3fc549
muhammadabdilah <Abilajah9@gmail.com>
2021-06-14 22:01:18 +0700

    Modul D : mencetak Struk

---
+++ ModulDMencetakStruk/Pesanan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+
+public class Pesanan {
+
+    private Menu menu;
+    private int jumlah;
+    private String keterangan;
+
+    public Pesanan(Menu menu, int jumlah) {
+        this.menu = menu;
+        this.jumlah = jumlah;
+    }
+
+    public Menu getMenu(){
+        return menu;
+    }
+
+    public int getJumlah(){
+        return jumlah;
+    }
+
+    public void setKeterangan(String keterangan) {}
+
+    public String getKeterangan() {return "";}
+
+} 
