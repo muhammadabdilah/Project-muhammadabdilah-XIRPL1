de41b3dfea7bbc8183c798cdd23505d5a898e9d6
muhammadabdilah <Abilajah9@gmail.com>
2021-06-14 20:55:33 +0700

    ImplementasiKelas Ramen, Kuah, Toping dan Minuman

---
+++ ImplementasiKelasRamenKuahTopingMinuman/Minuman.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+public class Minuman extends Menu{
+    public Minuman(String nama_minuman, double harga) { 
+        setNama_menu(nama_minuman);
+        setHarga(harga);
+        setKategori("Minuman");
+    }
+}
